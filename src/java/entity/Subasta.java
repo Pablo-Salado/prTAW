18b447e95c2b1fb7d30227dce37996b02bd80289
PabloSalado <0619980048@uma.es>
2022-04-10 13:02:47 +0200

    Created entities and dao

---
+++ src/java/entity/Subasta.java
@@ -0,0 +1,193 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author X430F
+ */
+@Entity
+@Table(name = "subasta")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Subasta.findAll", query = "SELECT s FROM Subasta s")
+    , @NamedQuery(name = "Subasta.findByIdSUBASTA", query = "SELECT s FROM Subasta s WHERE s.idSUBASTA = :idSUBASTA")
+    , @NamedQuery(name = "Subasta.findByApertura", query = "SELECT s FROM Subasta s WHERE s.apertura = :apertura")
+    , @NamedQuery(name = "Subasta.findByCierre", query = "SELECT s FROM Subasta s WHERE s.cierre = :cierre")
+    , @NamedQuery(name = "Subasta.findByPujaMaxima", query = "SELECT s FROM Subasta s WHERE s.pujaMaxima = :pujaMaxima")
+    , @NamedQuery(name = "Subasta.findByPrecioInicial", query = "SELECT s FROM Subasta s WHERE s.precioInicial = :precioInicial")
+    , @NamedQuery(name = "Subasta.findBySUBASTAcol", query = "SELECT s FROM Subasta s WHERE s.sUBASTAcol = :sUBASTAcol")})
+public class Subasta implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idSUBASTA")
+    private Integer idSUBASTA;
+    @Column(name = "APERTURA")
+    @Temporal(TemporalType.DATE)
+    private Date apertura;
+    @Column(name = "CIERRE")
+    @Temporal(TemporalType.DATE)
+    private Date cierre;
+    @Size(max = 45)
+    @Column(name = "PUJA_MAXIMA")
+    private String pujaMaxima;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "PRECIO_INICIAL")
+    private Double precioInicial;
+    @Size(max = 45)
+    @Column(name = "SUBASTAcol")
+    private String sUBASTAcol;
+    @JoinColumn(name = "PRODUCTO", referencedColumnName = "idPRODUCTO")
+    @ManyToOne(optional = false)
+    private Producto producto;
+    @JoinColumn(name = "VENDEDOR", referencedColumnName = "idUSUARIO")
+    @ManyToOne(optional = false)
+    private Usuario vendedor;
+    @JoinColumn(name = "COMPRADOR", referencedColumnName = "idUSUARIO")
+    @ManyToOne(optional = false)
+    private Usuario comprador;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "subasta")
+    private List<Producto> productoList;
+
+    public Subasta() {
+    }
+
+    public Subasta(Integer idSUBASTA) {
+        this.idSUBASTA = idSUBASTA;
+    }
+
+    public Integer getIdSUBASTA() {
+        return idSUBASTA;
+    }
+
+    public void setIdSUBASTA(Integer idSUBASTA) {
+        this.idSUBASTA = idSUBASTA;
+    }
+
+    public Date getApertura() {
+        return apertura;
+    }
+
+    public void setApertura(Date apertura) {
+        this.apertura = apertura;
+    }
+
+    public Date getCierre() {
+        return cierre;
+    }
+
+    public void setCierre(Date cierre) {
+        this.cierre = cierre;
+    }
+
+    public String getPujaMaxima() {
+        return pujaMaxima;
+    }
+
+    public void setPujaMaxima(String pujaMaxima) {
+        this.pujaMaxima = pujaMaxima;
+    }
+
+    public Double getPrecioInicial() {
+        return precioInicial;
+    }
+
+    public void setPrecioInicial(Double precioInicial) {
+        this.precioInicial = precioInicial;
+    }
+
+    public String getSUBASTAcol() {
+        return sUBASTAcol;
+    }
+
+    public void setSUBASTAcol(String sUBASTAcol) {
+        this.sUBASTAcol = sUBASTAcol;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Usuario getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(Usuario vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public Usuario getComprador() {
+        return comprador;
+    }
+
+    public void setComprador(Usuario comprador) {
+        this.comprador = comprador;
+    }
+
+    @XmlTransient
+    public List<Producto> getProductoList() {
+        return productoList;
+    }
+
+    public void setProductoList(List<Producto> productoList) {
+        this.productoList = productoList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idSUBASTA != null ? idSUBASTA.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Subasta)) {
+            return false;
+        }
+        Subasta other = (Subasta) object;
+        if ((this.idSUBASTA == null && other.idSUBASTA != null) || (this.idSUBASTA != null && !this.idSUBASTA.equals(other.idSUBASTA))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Subasta[ idSUBASTA=" + idSUBASTA + " ]";
+    }
+    
+}
---
+++ src/java/entity/Usuario.java
@@ -0,0 +1,179 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author X430F
+ */
+@Entity
+@Table(name = "usuario")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Usuario.findAll", query = "SELECT u FROM Usuario u")
+    , @NamedQuery(name = "Usuario.findByIdUSUARIO", query = "SELECT u FROM Usuario u WHERE u.idUSUARIO = :idUSUARIO")
+    , @NamedQuery(name = "Usuario.findByNombre", query = "SELECT u FROM Usuario u WHERE u.nombre = :nombre")
+    , @NamedQuery(name = "Usuario.findByEpellidos", query = "SELECT u FROM Usuario u WHERE u.epellidos = :epellidos")
+    , @NamedQuery(name = "Usuario.findBySexo", query = "SELECT u FROM Usuario u WHERE u.sexo = :sexo")
+    , @NamedQuery(name = "Usuario.findByPassword", query = "SELECT u FROM Usuario u WHERE u.password = :password")
+    , @NamedQuery(name = "Usuario.findByEmail", query = "SELECT u FROM Usuario u WHERE u.email = :email")})
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idUSUARIO")
+    private Integer idUSUARIO;
+    @Size(max = 45)
+    @Column(name = "NOMBRE")
+    private String nombre;
+    @Size(max = 45)
+    @Column(name = "EPELLIDOS")
+    private String epellidos;
+    @Size(max = 1)
+    @Column(name = "SEXO")
+    private String sexo;
+    @Size(max = 45)
+    @Column(name = "PASSWORD")
+    private String password;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Size(max = 45)
+    @Column(name = "EMAIL")
+    private String email;
+    @ManyToMany(mappedBy = "usuarioList")
+    private List<Producto> productoList;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "vendedor")
+    private List<Subasta> subastaList;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "comprador")
+    private List<Subasta> subastaList1;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer idUSUARIO) {
+        this.idUSUARIO = idUSUARIO;
+    }
+
+    public Integer getIdUSUARIO() {
+        return idUSUARIO;
+    }
+
+    public void setIdUSUARIO(Integer idUSUARIO) {
+        this.idUSUARIO = idUSUARIO;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEpellidos() {
+        return epellidos;
+    }
+
+    public void setEpellidos(String epellidos) {
+        this.epellidos = epellidos;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @XmlTransient
+    public List<Producto> getProductoList() {
+        return productoList;
+    }
+
+    public void setProductoList(List<Producto> productoList) {
+        this.productoList = productoList;
+    }
+
+    @XmlTransient
+    public List<Subasta> getSubastaList() {
+        return subastaList;
+    }
+
+    public void setSubastaList(List<Subasta> subastaList) {
+        this.subastaList = subastaList;
+    }
+
+    @XmlTransient
+    public List<Subasta> getSubastaList1() {
+        return subastaList1;
+    }
+
+    public void setSubastaList1(List<Subasta> subastaList1) {
+        this.subastaList1 = subastaList1;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idUSUARIO != null ? idUSUARIO.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Usuario)) {
+            return false;
+        }
+        Usuario other = (Usuario) object;
+        if ((this.idUSUARIO == null && other.idUSUARIO != null) || (this.idUSUARIO != null && !this.idUSUARIO.equals(other.idUSUARIO))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Usuario[ idUSUARIO=" + idUSUARIO + " ]";
+    }
+    
+}
